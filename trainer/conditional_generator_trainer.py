35c8deb2d4cd74f15ac3e4c2a5616e8d68bb6549
anjd <ite.anjd@outlook.com>
2018-04-15 18:06:42 +0300

    testdataset

--- trainer/conditional_generator_trainer.py
+++ trainer/conditional_generator_trainer.py
@@ -1,27 +1,27 @@
 import os
-import torch
 from os import cpu_count
 
+import torch
 from torch import nn
 from torch.autograd import Variable
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
-from dataset.coco_dataset import CocoDataset
 from dataset.corpus import Corpus
-# from extractor.vgg_extractor import VggExtractor
+from dataset.test_dataset import TestDataset
+from extractor.vgg_extractor import VggExtractor
 from file_path_manager import FilePathManager
 from generator.conditional_generator import ConditionalGenerator
 
 if not os.path.exists(FilePathManager.resolve("models")):
     os.makedirs(FilePathManager.resolve("models"))
 
-# extractor = VggExtractor(use_gpu=True, pretrained=False)
+extractor = VggExtractor(use_gpu=True, pretrained=True)
 corpus = Corpus.load(FilePathManager.resolve("data/corpus.pkl"))
 print("Corpus loaded")
 
 batch_size = 2
-dataset = CocoDataset(corpus, evaluator=False)
+dataset = TestDataset(corpus, extractor=extractor, evaluator=False)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=cpu_count())
 
 generator = ConditionalGenerator(corpus).cuda()
