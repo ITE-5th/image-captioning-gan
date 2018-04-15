7abf35f02e6192707af41ebb5f69bab0830a59ce
anjd <ite.anjd@outlook.com>
2018-04-15 21:32:37 +0300

    fix typo

--- misc/fasttext_to_gensim.py
+++ misc/fasttext_to_gensim.py
@@ -7,4 +7,3 @@ from file_path_manager import FilePathManager
 if __name__ == '__main__':
     model = FastText.load_fasttext_format(FilePathManager.resolve("data/wiki.en"))
     model.save(FilePathManager.resolve("data/fasttext.model"))
-    FastText.
