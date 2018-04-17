Fix exploding gradient(and sum errors)

generator/conditional_generator.py
@@ -112,7 +112,7 @@ class ConditionalGenerator(nn.Module):
         for i in range(self.max_sentence_length):
             # store the result
             result[:, i] = inputs.squeeze(1)
-
+            inputs = Variable(inputs)
             _, hidden = self.lstm(inputs, hidden)
             outputs = self.output_linear(hidden[0]).squeeze(0)
             predicted = outputs.max(-1)[1]

trainer/conditional_generator_trainer.py
@@ -30,7 +30,7 @@ dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_worker
 generator = ConditionalGenerator(corpus).cuda()
 criterion = nn.CrossEntropyLoss(ignore_index=corpus.word_index(corpus.PAD)).cuda()
 optimizer = Adam(generator.parameters(), lr=0.0001, weight_decay=1e-5)
-
+nn.MSELoss
 epochs = 20
 
 start = time.time()