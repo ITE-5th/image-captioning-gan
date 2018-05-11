bdf9f6502137fe22ef0e4f978a3139a54976d1a4
anjd <ite.anjd@outlook.com>
2018-05-11 06:04:23 +0300

    vectorized rollout

--- policy_gradient/rollout.py
+++ policy_gradient/rollout.py
@@ -15,33 +15,33 @@ class Rollout:
         self.max_sentence_length = max_sentence_length
         self.output_linear = None
 
-    def reward(self, generated, image_features, hidden, monte_carlo_count, evaluator):
+    def reward(self, generated, image_features, hidden, monte_carlo_count, evaluator, steps=1):
+        assert monte_carlo_count % steps == 0, "Monte Carlo Count can't be divided by Steps"
+        monte_carlo_count //= steps
+
         with torch.no_grad():
             batch_size = generated.size(0)
-            embedding_dim = generated.shape[-1]
             result = torch.zeros(batch_size, 1).cuda()
             remaining = self.max_sentence_length - generated.shape[1]
-            hidden = hidden.repeat(1, 1, monte_carlo_count).view(batch_size, -1, embedding_dim)
-            generated = generated.repeat(1, 1, monte_carlo_count).view(batch_size, -1, embedding_dim)
-            inputs = generated[:, -1].view(batch_size, 1, -1)
-            current_generated = generated
-            for i in range(remaining):
-                _, hidden = self.lstm(inputs, hidden)
-                outputs = self.output_linear(hidden[0]).squeeze(0)
-                outputs = F.softmax(outputs, -1)
-                predicted = outputs.multinomial(1)
-                # embed the next inputs, unsqueeze is required cause of shape (batch_size, 1, embedding_size)
-                inputs = self.embed.word_embeddings_from_indices(predicted.view(-1).cpu().data.numpy()).unsqueeze(
-                    1).cuda()
-                current_generated = torch.cat([current_generated, inputs], dim=1)
-            image_features = image_features.repeat(1, monte_carlo_count).view(batch_size, -1)
-            reward = evaluator(image_features, current_generated)
-            reward = torch.tensor([reward[i:i + monte_carlo_count].sum() for i in
-                                   range(0, batch_size * monte_carlo_count, monte_carlo_count)]).view(batch_size,
-                                                                                                      -1).cuda()
-            reward = reward.view(batch_size, -1)
-            result += reward
-            result /= monte_carlo_count
+            h, c = hidden
+            generated = generated.repeat(monte_carlo_count, 1, 1)
+            for _ in range(steps):
+                hidden = (h.repeat(1, monte_carlo_count, 1), c.repeat(1, monte_carlo_count, 1))
+                inputs = generated[:, -1].unsqueeze(1)
+                current_generated = generated
+                for i in range(remaining):
+                    _, hidden = self.lstm(inputs, hidden)
+                    outputs = self.output_linear(hidden[0]).squeeze(0)
+                    outputs = F.softmax(outputs, -1)
+                    predicted = outputs.multinomial(1)
+                    # embed the next inputs, unsqueeze is required cause of shape (batch_size, 1, embedding_size)
+                    inputs = self.embed.word_embeddings_from_indices(predicted.view(-1).cpu().data.numpy()).unsqueeze(
+                        1).cuda()
+                    current_generated = torch.cat([current_generated, inputs], dim=1)
+                reward = evaluator(image_features.repeat(monte_carlo_count, 1), current_generated)
+                reward = reward.view(batch_size, monte_carlo_count, -1).sum(1)
+                result += reward
+                result /= monte_carlo_count
             return result
 
     def update(self, original_model):
